f3afff935531db1095279e7a114092e629996e61
Jason Ramapuram <jason.ramapuram@gmail.com>
2018-01-29 02:01:04 +0100

    use kl from torch 0.4 and use softplus

--- models/isotropic_gaussian.py
+++ models/isotropic_gaussian.py
@@ -35,31 +35,21 @@ class IsotropicGaussian(nn.Module):
         feature_size = logits.size(-1)
         assert feature_size % 2 == 0
         mu = logits[:, 0:int(feature_size/2)]
-        #sigma = F.softplus(logits[:, int(feature_size/2):])
-        sigma = logits[:, int(feature_size/2):]
+        sigma = F.softplus(logits[:, int(feature_size/2):])
+        #sigma = logits[:, int(feature_size/2):]
         return self._reparametrize_gaussian(mu, sigma)
 
     @staticmethod
-    def _kld_gaussian_N_0_1(mu, logvar, feat_size, batch_size):
-        # standard_normal = D.Normal(zeros_like(mu), ones_like(logvar))
-        # normal = D.Normal(mu, logvar)
-        # return torch.sum(D.kl_divergence(normal, standard_normal), dim=-1)
-
-        # see Appendix B from VAE paper:
-        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-        # https://arxiv.org/abs/1312.6114
-        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-        # kld_element = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-        kld_element = -0.5 * torch.sum(1.0 + logvar - mu.pow(2)
-                                       - logvar.exp() - F.softplus(logvar), dim=-1)
-        #kld_element /= batch_size * feat_size
-        return kld_element
+    def _kld_gaussian_N_0_1(mu, logvar, cuda=False):
+        standard_normal = D.Normal(zeros_like(mu, cuda),
+                                   ones_like(logvar, cuda))
+        normal = D.Normal(mu, logvar)
+        return torch.sum(D.kl_divergence(normal, standard_normal), dim=-1)
 
     def kl(self, dist_a):
         return IsotropicGaussian._kld_gaussian_N_0_1(
             dist_a['gaussian']['mu'], dist_a['gaussian']['logvar'],
-            batch_size=dist_a['gaussian']['mu'].size(0),
-            feat_size=int(np.prod(self.config['img_shp']))
+            cuda=self.config['cuda']
         )
 
     def forward(self, logits):

add load and save functions; fix fid sizing; return non-summed kl

main.py
@@ -18,7 +18,7 @@ from helpers.grapher import Grapher
 from helpers.utils import to_data, softmax_accuracy, expand_dims, \
     int_type, float_type, long_type, add_weight_norm, ones_like, \
     squeeze_expand_dim, frechet_gauss_gauss, frechet_gauss_gauss_np, \
-    append_to_csv, num_samples_in_loader
+    append_to_csv, num_samples_in_loader, zero_pad_smaller_cat
 
 parser = argparse.ArgumentParser(description='LifeLong VAE Pytorch')
 
@@ -39,7 +39,9 @@ parser.add_argument('--download', type=int, default=1,
 parser.add_argument('--data-dir', type=str, default='./.datasets', metavar='DD',
                     help='directory which contains input data')
 parser.add_argument('--calculate-fid', action='store_true',
-                    help='calculate FID score (default: True)')
+                    help='calculate FID score (default: False)')
+parser.add_argument('--early-stop', action='store_true',
+                    help='enable early stopping (default: False)')
 
 # Model parameters
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
@@ -66,6 +68,8 @@ parser.add_argument('--disable-regularizers', action='store_true',
                     help='disables mutual info and consistency regularizers')
 parser.add_argument('--disable-sequential', action='store_true',
                     help='enables standard batch training')
+parser.add_argument('--disable-student-teacher', action='store_true',
+                    help='uses a standard VAE without Student-Teacher architecture')
 
 # Optimizer
 parser.add_argument('--optimizer', type=str, default="adamnorm",
@@ -141,7 +145,13 @@ def train(epoch, model, fisher, optimizer, data_loader, grapher):
                 100. * batch_idx * len(data) / num_samples,
                 loss['loss_mean'].data[0], loss['kld_mean'].data[0], loss['nll_mean'].data[0]))
 
-            register_plots(loss, grapher, epoch=TOTAL_ITER)
+            # gether tau val for discrete annealing if it exists
+            tau_val = output_map['student']['params']['discrete']['tau_scalar'] \
+                      if args.reparam_type == 'discrete' \
+                         or args.reparam_type == 'mixture' else None
+
+            # plot images and lines
+            register_plots(loss, grapher, epoch=TOTAL_ITER, tau=tau_val)
             register_images(output_map['student']['x_reconstr'],
                             output_map['augmented']['data'],
                             grapher)
@@ -151,7 +161,7 @@ def train(epoch, model, fisher, optimizer, data_loader, grapher):
         TOTAL_ITER += 1
 
 
-def register_plots(loss, grapher, epoch, prefix='train'):
+def register_plots(loss, grapher, epoch, tau=None, prefix='train'):
     grapher.register_single({'%s_loss' % prefix: [[epoch], [loss['loss_mean'].data[0]]]},
                             plot_type='line')
     grapher.register_single({'%s_kld' % prefix: [[epoch], [loss['kld_mean'].data[0]]]},
@@ -164,15 +174,18 @@ def register_plots(loss, grapher, epoch, prefix='train'):
                             plot_type='line')
     grapher.register_single({'%s_elbo' % prefix: [[epoch], [loss['elbo_mean'].data[0]]]},
                             plot_type='line')
-    if 'discrete' in loss:
-        grapher.register_single(
-            {'%s_temperature' % prefix: [[epoch], [loss['discrete']['temperature'].data[0]]]},
-            plot_type='line')
+
+    if 'ewc_mean' in loss:
+        grapher.register_single({'%s_ewc' % prefix: [[epoch], [loss['ewc_mean'].data[0]]]},
+                                plot_type='line')
 
     if 'posterior_regularizer_mean' in loss:
         grapher.register_single(
             {'%s_posterior_reg' % prefix: [[epoch], [loss['posterior_regularizer_mean'].data[0]]]},
             plot_type='line')
+
+    if tau:
+        grapher.register_single({'%s_tau' % prefix: [[epoch], [tau]]},plot_type='line')
 
 
 def register_images(reconstr_x, data, grapher, prefix="train"):
@@ -225,14 +238,23 @@ def test(epoch, model, data_loader, grapher):
         loss_map['kld_mean'].data[0],
         loss_map['nll_mean'].data[0]))
 
+    # gather the tau value for the annealing schedule
+    tau_val = output_map['student']['params']['discrete']['tau_scalar'] \
+              if args.reparam_type == 'discrete' \
+                 or args.reparam_type == 'mixture' else None
 
     # plot the test accuracy and loss
-    register_plots(loss_map, grapher, epoch=epoch, prefix='test')
+    register_plots(loss_map, grapher, epoch=epoch, tau=tau_val, prefix='test')
     register_images(output_map['student']['x_reconstr'],
                     output_map['augmented']['data'],
                     grapher, 'test')
     grapher.show()
-    return loss_map['elbo_mean'].detach().data[0] # return this for early stopping
+
+    # return this for early stopping
+    loss_val = loss_map['elbo_mean'].detach().data[0]
+    loss_map.clear()
+    output_map.clear()
+    return loss_val
 
 
 def calculate_fid_between_models(fid_model, model, data_loader,
@@ -285,20 +307,9 @@ def calculate_fid_from_generated_images(fid_model, model, data_loader, fid_layer
 
     with torch.no_grad():
         synthetic = [model.generate_synthetic_samples(model.student, args.batch_size)
-                                 for _ in range(num_synthetic + 1)]
-        #synthetic = torch.cat(synthetic, 0)[0:num_test_samples]
-        #print("synthetic_samples = ", synthetic.size())
-
-        # keep track of the features for later use
-        # synthetic_features, test_features = [], []
-
+                     for _ in range(num_synthetic + 1)]
         for (data, _), generated in zip(data_loader.test_loader, synthetic):
             data = Variable(data).cuda() if args.cuda else Variable(data)
-            # batch_size = data.size(0)
-            # extract features from both the models
-            # fid += frechet_gauss_gauss_np(fid_submodel(generated).view(batch_size, -1).cpu().numpy(),
-            #                               fid_submodel(data).view(batch_size, -1).cpu().numpy()
-            # )
             fid += frechet_gauss_gauss(
                 D.Normal(torch.mean(fid_submodel(generated), dim=0), torch.var(fid_submodel(generated), dim=0)),
                 D.Normal(torch.mean(fid_submodel(data), dim=0), torch.var(fid_submodel(data), dim=0))
@@ -310,18 +321,6 @@ def calculate_fid_from_generated_images(fid_model, model, data_loader, fid_layer
         (num_test_samples // args.batch_size) * args.batch_size, frechet_dist)
     )
     return frechet_dist
-
-    #         synthetic_features.append(fid_submodel(generated).view(batch_size, -1))
-    #         test_features.append(fid_submodel(data).view(batch_size, -1))
-
-    # # concat and pull to CPU
-    # synthetic_features = torch.cat(synthetic_features, 0).cpu().numpy()
-    # test_features = torch.cat(test_features, 0).cpu().numpy()
-
-    # # calculate the CPU frechet score
-    # frechet_dist = frechet_gauss_gauss_np(synthetic_features, test_features)
-    # print("frechet distance: ", frechet_dist)
-    # return frechet_dist
 
 
 def generate(student_teacher, grapher, name='teacher'):
@@ -339,17 +338,17 @@ def generate(student_teacher, grapher, name='teacher'):
         grapher.register_single({'generated_%s'%name: gen}, plot_type='imgs')
 
         # sequential generation for discrete and mixture reparameterizations
-        # if args.reparam_type == 'mixture' or args.reparam_type == 'discrete':
-        #     gen = student_teacher.generate_synthetic_sequential_samples(model[name]).detach()
-        #     gen = torch.min(gen, ones_like(gen, args.cuda))
-        #     grapher.register_single({'sequential_generated_%s'%name: gen}, plot_type='imgs')
+        if args.reparam_type == 'mixture' or args.reparam_type == 'discrete':
+            gen = student_teacher.generate_synthetic_sequential_samples(model[name]).detach()
+            gen = torch.min(gen, ones_like(gen, args.cuda))
+            grapher.register_single({'sequential_generated_%s'%name: gen}, plot_type='imgs')
 
 
 def get_model_and_loader():
     ''' helper to return the model and the loader '''
     if args.disable_sequential: # vanilla batch training
         loaders = get_loader(args)
-        loaders = list(loaders) if not isinstance(loaders, list) else loaders
+        loaders = [loaders] if not isinstance(loaders, list) else loaders
     else: # classes split
         loaders = get_sequential_data_loaders(args, num_classes=10)
 
@@ -371,22 +370,15 @@ def get_model_and_loader():
 
 
 def estimate_fisher(model, data_loader, sample_size=1024):
-    # modified from github user kuc2477
+    # modified from github user kuc2477's code
     # sample loglikelihoods from the dataset.
     loglikelihoods = []
     for x, _ in data_loader.train_loader:
         x = Variable(x).cuda() if args.cuda else Variable(x)
-        # encoded = model.teacher.encode(x)
-        # reparam = model.teacher.reparameterizer
-        # _, params = reparam(encoded)
-
-        # loglikelihoods.append(
-        #     reparam.log_likelihood(params['z'], params)[range(args.batch_size)]
-        # )
-        reconstr_x, _ = model.teacher(x)
-
+        reconstr_x, params = model.teacher(x)
+        loss_teacher = model.teacher.loss_function(reconstr_x, x, params)
         loglikelihoods.append(
-            model.teacher.nll(reconstr_x, x)
+            loss_teacher['loss']
         )
         if len(loglikelihoods) >= sample_size // args.batch_size:
             break
@@ -408,6 +400,62 @@ def lazy_generate_modules(model, img_shp):
     data = float_type(args.cuda)(args.batch_size, *img_shp).normal_()
     model(Variable(data))
 
+def calculate_consistency(model, loader):
+    ''' \sum z_d(teacher) == z_d(student) forall test samples '''
+    consistency = 0.0
+
+    if model.current_model > 0 and (args.reparam_type == 'mixture'
+                                    or args.reparam_type == 'discrete'):
+        model.eval() # prevents data augmentation
+        consistency = []
+
+        for img, _ in loader.test_loader:
+            with torch.no_grad():
+                img = Variable(img).cuda() if args.cuda else Variable(img)
+
+                output_map = model(img)
+                teacher_posterior = output_map['teacher']['params']['discrete']['logits']
+                student_posterior = output_map['student']['params']['discrete']['logits']
+                teacher_posterior = F.softmax(teacher_posterior, dim=-1)
+                student_posterior = F.softmax(student_posterior, dim=-1)
+                teacher_posterior, student_posterior \
+                    = zero_pad_smaller_cat(teacher_posterior, student_posterior,
+                                           cuda=args.cuda)
+
+                correct = to_data(teacher_posterior).max(1)[1] \
+                          == to_data(student_posterior).max(1)[1]
+                consistency.append(torch.mean(correct.type(float_type(args.cuda))))
+                # print("teacher = ", teacher_posterior)
+                # print("student = ", student_posterior)
+                # print("consistency[-1]=", correct)
+
+        num_test_samples = num_samples_in_loader(loader.test_loader)
+        consistency = np.mean(consistency)
+        print("Consistency [#samples: {}]: {}\n".format(num_test_samples,
+                                                      consistency))
+    return np.asarray([consistency])
+
+
+def calculate_fid(fid_model, model, loader, grapher, across_models=False):
+    # evaluate and cache away the FID score
+    fid = np.inf
+    if args.calculate_fid:
+        if across_models:
+            # weird way
+            fid = calculate_fid_between_models(fid_model, model, loader)
+        else:
+            # standard way
+            fid = calculate_fid_from_generated_images(fid_model, model, loader)
+
+        grapher.vis.text(str(fid), opts=dict(title="FID"))
+
+    return fid
+
+def test_and_generate(epoch, model, loader, grapher):
+    test_loss = test(epoch, model, loader, grapher)
+    generate(model, grapher, 'student') # generate student samples
+    generate(model, grapher, 'teacher') # generate teacher samples
+    return test_loss
 
 def run(args):
     # collect our model and data loader
@@ -429,40 +477,46 @@ def run(args):
 
     # main training loop
     for j, loader in enumerate(data_loaders):
-        num_epochs = args.epochs + np.random.randint(0, 13)
+        num_epochs = args.epochs #+ np.random.randint(0, 13)
         print("training current distribution for {} epochs".format(num_epochs))
-        early = EarlyStopping()
+        early = EarlyStopping(model, max_steps=50) if args.early_stop else None
 
+        test_loss = 0.
         for epoch in range(1, num_epochs + 1):
             train(epoch, model, fisher, optimizer, loader, grapher)
-            elbo = test(epoch, model, loader, grapher)
+            test_loss = test(epoch, model, loader, grapher)
+            if args.early_stop and early(test_loss):
+                early.restore() # restore and test+generate again
+                test_loss = test_and_generate(epoch, model, loader, grapher)
+                break
+
             generate(model, grapher, 'student') # generate student samples
             generate(model, grapher, 'teacher') # generate teacher samples
-            # if early(elbo):
-            #     break
 
-        # evaluate and cache away the FID score
+        # evaluate one-time metrics
+        append_to_csv([test_loss], "{}_test_elbo.csv".format(args.uid))
+        append_to_csv(calculate_consistency(model, loader),
+                      "{}_consistency.csv".format(args.uid))
         if args.calculate_fid:
-            fid = calculate_fid_from_generated_images(fid_model, model, loader)
-            grapher.vis.text(str(fid), opts=dict(title="FID"))
-            if args.uid:
-                filename = filename="{}.csv".format(args.uid)
-                append_to_csv(np.asarray(fid), filename)
+            append_to_csv(calculate_fid(fid_model, model, loader, grapher),
+                          "{}_fid.csv".format(args.uid))
 
         if j != len(data_loaders) - 1:
-            # spawn a new student & rebuild grapher
-            # we also pass the new model's parameters through
-            # a new optimizer
-            model.fork()
-            lazy_generate_modules(model, data_loaders[0].img_shp)
-            optimizer = build_optimizer(model.student)
-            grapher.save()
+            # spawn a new student & rebuild grapher; we also pass
+            # the new model's parameters through a new optimizer.
+            if not args.disable_student_teacher:
+                model.fork()
+                lazy_generate_modules(model, data_loaders[0].img_shp)
+                optimizer = build_optimizer(model.student)
+
+            grapher.save() # save to json after distributional interval
             grapher = Grapher(env=model.get_name(),
                               server=args.visdom_url,
                               port=args.visdom_port)
 
-            # calculate the fisher from the previous data loader
-            fisher = estimate_fisher(model, loader) if args.ewc else None
+            if args.ewc:
+                # calculate the fisher from the previous data loader
+                fisher = estimate_fisher(model, loader) if args.ewc else None
 
 
 if __name__ == "__main__":

models/fid.py
@@ -8,8 +8,10 @@ import torch.optim as optim
 from torch.autograd import Variable
 from collections import OrderedDict
 
+from optimizers.adamnormgrad import AdamNormGrad
 from models.layers import View, Identity, flatten_layers, EarlyStopping
 from datasets.loader import get_loader
+from datasets.utils import GenericLoader, simple_merger
 from helpers.utils import float_type, zeros_like, ones_like, \
     softmax_accuracy, check_or_create_dir
 
@@ -18,6 +20,7 @@ def build_optimizer(model, args):
     optim_map = {
         "rmsprop": optim.RMSprop,
         "adam": optim.Adam,
+        "adamnorm": AdamNormGrad,
         "adadelta": optim.Adadelta,
         "sgd": optim.SGD,
         "lbfgs": optim.LBFGS
@@ -107,25 +110,30 @@ def train_fid_model(reparameterizer_input_size,
                     args):
     ''' builds and trains a classifier '''
     loader = get_loader(args)
+    if isinstance(loader, list): # has a sequential loader
+        # loader = simple_merger(loader, args.batch_size, args.cuda)
+        loader = loader[0]
+
     model = FID(loader.img_shp,
                 loader.output_size,
-                reparameterizer_input_size,
-                reparameterizer_output_size,
+                reparameterizer_input_size=32,
+                reparameterizer_output_size=32,
                 kwargs=vars(args))
     if not model.model_exists:
         lazy_generate_modules(model, loader.img_shp,
                               args.batch_size, args.cuda)
         optimizer = build_optimizer(model, args)
-        early_stop = EarlyStopping(max_steps=10)
+        early_stop = EarlyStopping(model, max_steps=10)
 
         for epoch in range(1, args.epochs + 1):
             train(epoch, model, optimizer, loader, args)
             loss, _ = test(epoch, model, loader, args)
             if early_stop(loss):
+                early_stop.restore()
                 break
 
         # save the model
-        model.save_model()
+        model.save()
 
     return model
 
@@ -153,9 +161,9 @@ class FID(nn.Module):
         self.encoder = self.build_encoder()
         self.decoder = self.build_decoder()
         self.full_model = None
-        self.model_exists = self.load_model()
+        self.model_exists = self.load()
 
-    def load_model(self):
+    def load(self):
         # load the FID model if it exists
         if os.path.isdir(".models"):
             model_filename = os.path.join(".models", self.get_name() + ".th")
@@ -169,14 +177,13 @@ class FID(nn.Module):
 
         return False
 
-    def save_model(self):
+    def save(self, overwrite=False):
         # save the FID model if it doesnt exist
         check_or_create_dir(".models")
         model_filename = os.path.join(".models", self.get_name() + ".th")
-        if not os.path.isfile(model_filename):
+        if not os.path.isfile(model_filename) or overwrite:
             print("saving existing FID model")
             torch.save(self.state_dict(), model_filename)
-
 
     def get_name(self):
         full_hash_str = "_" + str(self.config['layer_type']) + \
@@ -196,8 +203,9 @@ class FID(nn.Module):
                                                      .replace('(', '') \
                                                      .replace(')', '') \
                                                      .replace('\'', '')
-        tasks_cleaned = [t.split('_')[1] if 'rotated' in t else t for t in self.config['task']]
-        return 'fid_' + '_'.join(tasks_cleaned) + full_hash_str
+        # tasks_cleaned = [t.split('_')[1] if 'rotated' in t else t for t in self.config['task']]
+        # return 'fid_' + '_'.join(tasks_cleaned) + full_hash_str
+        return 'fid_' + '_'.join(self.config['task']) + full_hash_str
 
 
     def build_encoder(self):

models/layers.py
@@ -1,3 +1,4 @@
+import os
 import math
 import torch
 import torch.nn as nn
@@ -8,6 +9,7 @@ from torchvision.models import resnet18
 from models.isotropic_gaussian import IsotropicGaussian
 from models.gumbel import GumbelSoftmax
 from models.mixture import Mixture
+from helpers.utils import check_or_create_dir
 
 
 class View(nn.Module):
@@ -53,17 +55,25 @@ class Submodel(nn.Module):
 
 
 class EarlyStopping(object):
-    def __init__(self, max_steps=10):
+    def __init__(self, model, max_steps=10, save_best=True):
         self.max_steps = max_steps
+        self.model = model
+        self.save_best = save_best
+
         self.loss = 0.0
         self.iteration = 0
         self.stopping_step = 0
         self.best_loss = np.inf
 
+    def restore(self):
+        self.model.load()
+
     def __call__(self, loss):
         if (loss < self.best_loss):
             self.stopping_step = 0
             self.best_loss = loss
+            if self.save_best:
+                self.model.save(overwrite=True)
         else:
             self.stopping_step += 1
 

models/student_teacher.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import pprint
+import os
 import torch
 import numpy as np
 import torch.nn as nn
@@ -8,10 +8,10 @@ import torch.distributions as D
 from torch.autograd import Variable
 from copy import deepcopy
 
-from models.gumbel import GumbelSoftmax
 from models.vae import VAE
 from helpers.utils import expand_dims, long_type, squeeze_expand_dim, \
-    ones_like, float_type, pad, invert_shuffle, one_hot_np
+    ones_like, float_type, pad, invert_shuffle, one_hot_np, \
+    zero_pad_smaller_cat, check_or_create_dir
 
 
 def detach_from_graph(param_map):
@@ -47,7 +47,7 @@ def kl_categorical_categorical(dist_a, dist_b, rnd_perm, from_index=0, cuda=Fals
     return torch.sum(dist_a_softmax * delta_log_probs1, dim=-1)
 
 
-# def kl_categorical_categorical(dist_a, dist_b, from_index=0, cuda=False):
+# def kl_categorical_categorical(dist_a, dist_b, rnd_perm, from_index=0, cuda=False):
 #     dist_a_softmax = F.softmax(dist_a['logits'][from_index:], dim=-1)
 #     dist_b_softmax = F.softmax(dist_b['logits'][from_index:], dim=-1)
 #     dist_a_softmax, dist_b_softmax \
@@ -79,24 +79,11 @@ def kl_isotropic_gauss_gauss(dist_a, dist_b, rnd_perm=None):
     return torch.sum(D.kl_divergence(n0, n1), dim=-1)
 
 
-def zero_pad_smaller_cat(cat1, cat2, cuda=False):
-    c1shp = cat1.size()
-    c2shp = cat2.size()
-    diff = abs(c1shp[1] - c2shp[1])
-
-    # blend in extra zeros appropriately
-    if c1shp[1] > c2shp[1]:
-        cat2 = torch.cat(
-            [cat2,
-             Variable(float_type(cuda)(c2shp[0], diff).zero_())],
-            dim=-1)
-    elif c2shp[1] > c1shp[1]:
-        cat1 = torch.cat(
-            [cat1,
-             Variable(float_type(cuda)(c1shp[0], diff).zero_())],
-            dim=-1)
-
-    return [cat1, cat2]
+def lazy_generate_modules(model, img_shp, batch_size, cuda):
+    ''' Super hax, but needed for building lazy modules '''
+    model.eval()
+    data = float_type(cuda)(batch_size, *img_shp).normal_()
+    model(Variable(data))
 
 
 class StudentTeacher(nn.Module):
@@ -113,6 +100,28 @@ class StudentTeacher(nn.Module):
 
         # grab the meta config and print for
         self.config = kwargs['kwargs']
+
+    def load(self):
+        # load the model if it exists
+        if os.path.isdir(".models"):
+            model_filename = os.path.join(".models", self.get_name() + ".th")
+            if os.path.isfile(model_filename):
+                print("loading existing student-teacher model")
+                lazy_generate_modules(self, self.student.input_shape,
+                                      self.config['batch_size'],
+                                      self.config['cuda'])
+                self.load_state_dict(torch.load(model_filename))
+                return True
+
+        return False
+
+    def save(self, overwrite=False):
+        # save the model if it doesnt exist
+        check_or_create_dir(".models")
+        model_filename = os.path.join(".models", self.get_name() + ".th")
+        if not os.path.isfile(model_filename) or overwrite:
+            print("saving existing student-teacher model")
+            torch.save(self.state_dict(), model_filename)
 
     def get_name(self):
         #teacher_name = "teacher_" + self.teacher.get_name() if self.teacher else "teacher_none"
@@ -147,7 +156,7 @@ class StudentTeacher(nn.Module):
             diff = int(np.abs(vae_loss['loss'].size(0) - posterior_regularizer.size(0)))
             posterior_regularizer = pad(posterior_regularizer,
                                         diff,
-                                        dim=-1,
+                                        dim=0,
                                         prepend=True,
                                         cuda=self.config['cuda'])
             # posterior_regularizer = posterior_regularizer[self.rnd_perm]
@@ -157,11 +166,14 @@ class StudentTeacher(nn.Module):
 
         return vae_loss
 
-    def _ewc(self, fisher_matrix, gamma=3.0):
+    def _ewc(self, fisher_matrix, gamma=30):
         losses = []
         for (nt, pt), (ns, ps), (nf, fish) in zip(self.teacher.named_parameters(),
                                                   self.student.named_parameters(),
                                                   fisher_matrix.items()):
+            # print("f {} * (t {} - s {})".format(nf, nt, ns))
+            # print("f {} * (t {} - s {})".format(fish.size(), pt.size(), ps.size()))
+            # print("f {} * (t {} - s {})".format(type(fish), type(pt), type(ps)))
             if pt.size() == ps.size():
                 losses.append(torch.sum(fish * (pt - ps)**2))
 
@@ -173,9 +185,7 @@ class StudentTeacher(nn.Module):
         vae_loss = self.student.loss_function(output_map['student']['x_reconstr_logits'],
                                               output_map['augmented']['data'],
                                               output_map['student']['params'])
-        if 'teacher' in output_map \
-           and not self.config['disable_regularizers'] \
-           and fisher_matrix is not None:
+        if 'teacher' in output_map and fisher_matrix is not None:
             ewc = self._ewc(fisher_matrix)
             vae_loss['ewc_mean'] = ewc
             vae_loss['loss_mean'] = torch.mean(vae_loss['loss']) + ewc
@@ -203,55 +213,34 @@ class StudentTeacher(nn.Module):
         return [src, dest]
 
     def fork(self):
+        # copy the old student into the teacher
         config_copy = deepcopy(self.student.config)
         config_copy['discrete_size'] += 1
         self.teacher = deepcopy(self.student)
 
-        # self.teacher = VAE(input_shape=self.student.input_shape,
-        #                    **{'kwargs': self.student.config}
-        # )
-        # data = float_type(self.config['cuda'])(self.student.config['batch_size'],
-        #                                        *self.student.input_shape).normal_()
-        # self.teacher(Variable(data))
-        # self.student, self.teacher \
-        #     = self.copy_model(self.student,
-        #                       self.teacher,
-        #                       disable_dst_grads=True)
-
-        # student_params = list(self.student.parameters())
-        # teacher_params = list(self.teacher.parameters())
-        # for i in range(len(student_params)):
-        #     teacher_params[i].data[:] = student_params[i].data[:].clone()
-        #     teacher_params[i].requires_grad = False
-
+        # create a new student
         self.student = VAE(input_shape=self.teacher.input_shape,
+                           num_current_model=self.current_model+1,
                            **{'kwargs': config_copy}
         )
 
         # forward pass once to build lazy modules
-        # data = float_type(self.config['cuda'])(self.student.config['batch_size'],
-        #                                        *self.student.input_shape).normal_()
-        # self.student(Variable(data))
+        data = float_type(self.config['cuda'])(self.student.config['batch_size'],
+                                               *self.student.input_shape).normal_()
+        self.student(Variable(data))
 
-        # # copy teacher params into student while
-        # # increasing the dimensionality of the projection
-        # self.teacher, self.student \
-        #     = self.copy_model(self.teacher, self.student, disable_dst_grads=False)
-
-        # copy the teacher weights into the student model
-        # for student_param, teacher_param in zip(self.student.parameters(),
-        #                                         self.teacher.parameters()):
-        #     student_size = student_param.size()
-        #     teacher_size = teacher_param.size()
-        #     if student_size == teacher_size:
-        #         student_param.data = teacher_param.data.clone()
+        # copy teacher params into student while
+        # omitting the projection weights
+        self.teacher, self.student \
+            = self.copy_model(self.teacher, self.student, disable_dst_grads=False)
 
         # update the current model's ratio
         self.current_model += 1
         self.ratio = self.current_model / (self.current_model + 1.0)
         num_teacher_samples = int(self.config['batch_size'] * self.ratio)
+        num_student_samples = max(self.config['batch_size'] - num_teacher_samples, 1)
         print("#teacher_samples: ", num_teacher_samples,
-              " | #student_samples: ", max(self.config['batch_size'] - num_teacher_samples, 1))
+              " | #student_samples: ", num_student_samples)
 
     def generate_synthetic_samples(self, model, batch_size):
         z_samples = model.reparameterizer.prior([batch_size,
@@ -263,17 +252,26 @@ class StudentTeacher(nn.Module):
             or self.config['reparam_type'] == 'discrete'
 
         # create a grid of one-hot vectors for displaying in visdom
-        num_discrete = model.reparameterizer.config['discrete_size']
-        discrete_indices = np.asarray([[i]*num_rows for i in np.arange(num_discrete)])
+        # uses one row for original dimension of discrete component
+        num_current_discrete = model.reparameterizer.config['discrete_size']
+        num_orig_discrete = num_current_discrete - model.num_current_model
+        discrete_indices = np.asarray([[np.random.randint(0, num_orig_discrete)]
+                                       for _ in range(num_rows)]).reshape(-1, num_rows)
+        if model.num_current_model > 0:
+            extra_discrete_indices = np.asarray([[i]*num_rows for i in range(num_orig_discrete,
+                                                                             num_current_discrete)])
+            discrete_indices = np.vstack([discrete_indices, extra_discrete_indices])
+
         discrete_indices = discrete_indices.reshape(-1)
         with torch.no_grad():
-            z_samples = Variable(torch.from_numpy(one_hot_np(num_discrete, discrete_indices)))
+            z_samples = Variable(torch.from_numpy(one_hot_np(num_current_discrete,
+                                                             discrete_indices)))
             z_samples = z_samples.type(float_type(self.config['cuda']))
 
             if self.config['reparam_type'] == 'mixture':
                 ''' add in the gaussian prior '''
                 z_gauss = model.reparameterizer.gaussian.prior(
-                    [num_rows*num_discrete, model.reparameterizer.gaussian.output_size]
+                    [z_samples.size(0), model.reparameterizer.gaussian.output_size]
                 )
                 z_samples = torch.cat([z_gauss, z_samples], dim=-1)
 
@@ -282,7 +280,7 @@ class StudentTeacher(nn.Module):
     def _augment_data(self, x):
         ''' return batch_size worth of samples that are augmented
             from the teacher model '''
-        if self.ratio == 1.0 or not self.training:
+        if self.ratio == 1.0 or not self.training: #or self.config['ewc']:
             return x   # base case
 
         batch_size = x.size(0)

models/vae.py
@@ -36,9 +36,10 @@ def log_logistic_256(x, mean, log_s, average=False, dim=None):
 
 
 class VAE(nn.Module):
-    def __init__(self, input_shape, activation_fn=nn.ELU, **kwargs):
+    def __init__(self, input_shape, activation_fn=nn.ELU, num_current_model=0, **kwargs):
         super(VAE, self).__init__()
         self.input_shape = input_shape
+        self.num_current_model = num_current_model
         self.activation_fn = activation_fn
         self.is_color = input_shape[0] > 1
         self.chans = 3 if self.is_color else 1
@@ -339,22 +340,11 @@ class VAE(nn.Module):
             x.view(batch_size, -1)
         )
         return -torch.sum(nll, dim=-1)
-        # assert x.size() == recon_x_logits.size()
-        # batch_size = recon_x_logits.size(0)
-        # recon_x_logits = recon_x_logits.view(batch_size, -1)
-        # x = x.view(batch_size, -1)
-        # max_val = (-recon_x_logits).clamp(min=0)
-        # nll = recon_x_logits - recon_x_logits * x \
-        #       + max_val + ((-max_val).exp() + (-recon_x_logits - max_val).exp()).log()
-        # return torch.sum(nll, dim=-1)
 
     def nll_clamp(self, x, recon):
         batch_size, num_half_chans = x.size(0), recon.size(1) // 2
         recon_mu = recon[:, 0:num_half_chans, :, :]
         recon_logvar = recon[:, num_half_chans:, :, :]
-
-        # if recon_sigma is None:
-        #     recon_sigma = ones_like(recon_mu, self.config['cuda'])
 
         return log_logistic_256(x.view(batch_size, -1),
                                 torch.clamp(recon_mu.view(batch_size, -1), min=0.+1./512., max=1.-1./512.),
@@ -380,10 +370,7 @@ class VAE(nn.Module):
         recon_mu = recon[:, 0:num_half_chans, :, :]
         recon_logvar = recon[:, num_half_chans:, :, :]
 
-        # if recon_logvar is None:
-        #     recon_logvar = ones_like(recon_mu, self.config['cuda'])
-
-        # XXX: currently broken
+        # XXX: currently broken, so set var to 1
         recon_logvar = ones_like(recon_mu, self.config['cuda'])
 
         nll = D.Normal(
@@ -392,34 +379,6 @@ class VAE(nn.Module):
             recon_logvar.view(batch_size, -1)
         ).log_prob(x.view(batch_size, -1))
         return -torch.sum(nll, dim=-1)
-
-
-    # def nll_gaussian(self, recon_x_logits, x, logvar=1):
-    #     # Helpers to get the gaussian log-likelihood pulled from tensorflow
-    #     # (https://github.com/tensorflow/tensorflow/blob/r1.2/tensorflow/python/ops/distributions/normal.py)
-    #     def _z(x, loc, scale, eps=1e-9):
-    #         """Standardize input `x` to a unit normal."""
-    #         return (x - loc) / (scale + eps)
-
-    #     # return -0.5 * ( log_var + K.square( sample - mean ) / K.exp( log_var ) )
-    #     def _log_unnormalized_prob(x, loc, scale):
-    #         return -0.5 * torch.pow(_z(x, loc, scale), 2)
-
-    #     def _log_normalization(scale, eps=1e-9):
-    #         if isinstance(scale, (float, int, np.float32, np.float64)):
-    #             return np.log(scale + eps) + 0.5 * np.log(2. * np.pi)
-
-    #         return torch.log(scale + eps) + 0.5 * np.log(2. * np.pi)
-
-    #     batch_size = x.size(0)
-    #     xflat = x.view(batch_size, -1)
-    #     locflat = F.sigmoid(recon_x_logits.view(batch_size, -1))
-    #     scaleflat = logvar if isinstance(logvar, (float, int, np.float32, np.float64)) \
-    #                 else logvar.view(batch_size, -1)
-    #     normalized_prob = _log_normalization(scale=scaleflat)
-    #     unnormalized_prob = _log_unnormalized_prob(xflat, loc=locflat, scale=scaleflat)
-    #     nll = unnormalized_prob - normalized_prob
-    #     return torch.sum(nll, dim=-1)
 
     def kld(self, dist_a):
         ''' accepts param maps for dist_a and dist_b,
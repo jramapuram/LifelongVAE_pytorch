fix two critical issues, kl sum and sign of mut info; add parameterizable ewc gamma; add results dierctories

main.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import numpy as np
 import pprint
@@ -16,7 +17,7 @@ from helpers.grapher import Grapher
 from helpers.fid import train_fid_model
 from helpers.metrics import calculate_consistency, calculate_fid, estimate_fisher
 from helpers.utils import float_type, ones_like, \
-    append_to_csv, num_samples_in_loader
+    append_to_csv, num_samples_in_loader, check_or_create_dir
 
 parser = argparse.ArgumentParser(description='LifeLong VAE Pytorch')
 
@@ -36,6 +37,8 @@ parser.add_argument('--download', type=int, default=1,
                     help='download dataset from s3 (default: 1)')
 parser.add_argument('--data-dir', type=str, default='./.datasets', metavar='DD',
                     help='directory which contains input data')
+parser.add_argument('--output-dir', type=str, default='./experiments', metavar='OD',
+                    help='directory which contains csv results')
 parser.add_argument('--calculate-fid-with', type=str, default=None,
                     help='enables FID calc & uses model conv/inceptionv3  (default: None)')
 parser.add_argument('--early-stop', action='store_true',
@@ -60,6 +63,8 @@ parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status')
 parser.add_argument('--ewc', action='store_true',
                     help='use the EWC regularizer instead')
+parser.add_argument('--ewc-gamma', type=float, default=10000,
+                    help='if using ewc this is the hyper-parameter (default: 10k)')
 parser.add_argument('--vae-type', type=str, default='parallel',
                     help='vae type [sequential or parallel] (default: parallel)')
 parser.add_argument('--disable-regularizers', action='store_true',
@@ -131,11 +136,11 @@ def train(epoch, model, fisher, optimizer, data_loader, grapher):
         # zero gradients on optimizer
         optimizer.zero_grad()
 
-        # run the VAE + the DNN on the latent space
+        # run the VAE and extract loss
         output_map = model(data)
         loss = model.loss_function(output_map, fisher)
 
-        # compute loss
+        # compute loss and do a backward pass
         loss['loss_mean'].backward()
         optimizer.step()
 
@@ -236,7 +241,7 @@ def test(epoch, model, data_loader, grapher):
     grapher.show()
 
     # return this for early stopping
-    loss_val = loss_map['elbo_mean'].detach().data[0]
+    loss_val = loss_map['elbo_mean'].detach().item()
     loss_map.clear()
     output_map.clear()
     return loss_val
@@ -340,9 +345,9 @@ def run(args):
 
     # main training loop
     for j, loader in enumerate(data_loaders):
-        num_epochs = args.epochs #+ np.random.randint(0, 13)
+        num_epochs = args.epochs # TODO: randomize epochs by: + np.random.randint(0, 13)
         print("training current distribution for {} epochs".format(num_epochs))
-        early = EarlyStopping(model, max_steps=50) if args.early_stop else None
+        early = EarlyStopping(model, max_steps=50, burn_in_interval=100) if args.early_stop else None
 
         test_loss = 0.
         for epoch in range(1, num_epochs + 1):
@@ -356,14 +361,16 @@ def run(args):
             generate(model, grapher, 'student') # generate student samples
             generate(model, grapher, 'teacher') # generate teacher samples
 
-        # evaluate one-time metrics
-        append_to_csv([test_loss], "{}_test_elbo.csv".format(args.uid))
+        # evaluate and save away one-time metrics
+        check_or_create_dir(os.path.join(args.output_dir))
+        append_to_csv([test_loss], os.path.join(args.output_dir, "{}_test_elbo.csv".format(args.uid)))
         append_to_csv(calculate_consistency(model, loader, args.reparam_type, args.vae_type, args.cuda),
-                      "{}_consistency.csv".format(args.uid))
+                      os.path.join(args.output_dir, "{}_consistency.csv".format(args.uid)))
         grapher.vis.text(pprint.PrettyPrinter(indent=4).pformat(model.student.config),
                          opts=dict(title="config"))
 
         if args.calculate_fid_with is not None:
+            # TODO: parameterize num fid samples, currently use less for inceptionv3 as it's COSTLY
             num_fid_samples = 4000 if args.calculate_fid_with != 'inceptionv3' else 1000
             append_to_csv(calculate_fid(fid_model=fid_model,
                                         model=model,
@@ -372,6 +379,7 @@ def run(args):
                                         cuda=args.cuda),
                           "{}_fid.csv".format(args.uid))
 
+        grapher.save() # save the remote visdom graphs
         if j != len(data_loaders) - 1:
             # spawn a new student & rebuild grapher; we also pass
             # the new model's parameters through a new optimizer.
@@ -382,14 +390,15 @@ def run(args):
                 print("there are {} params in the st-model and {} params in the student".format(
                     len(list(model.parameters())), len(list(model.student.parameters()))))
 
-            grapher.save() # save to json after distributional interval
             grapher = Grapher(env=model.get_name(),
                               server=args.visdom_url,
                               port=args.visdom_port)
 
             if args.ewc:
                 # calculate the fisher from the previous data loader
-                fisher = estimate_fisher(model, loader, args.batch_size, cuda=args.cuda) if args.ewc else None
+                fisher = estimate_fisher(model.teacher,
+                                         loader, args.batch_size,
+                                         cuda=args.cuda)
 
 
 if __name__ == "__main__":

models/reparameterizers/gumbel.py
@@ -66,8 +66,6 @@ class GumbelSoftmax(nn.Module):
         crossent_loss = -torch.sum(log_q_z_given_x * p_z, dim=1)
         ent_loss = -torch.sum(torch.log(p_z + eps) * p_z, dim=1)
         return crossent_loss + ent_loss
-        # return torch.mean(crossent_loss + ent_loss)
-        # return torch.mean(crossent_loss) + torch.mean(ent_loss)
 
     @staticmethod
     def _kld_categorical_uniform(log_q_z, eps=1e-9):
@@ -79,9 +77,9 @@ class GumbelSoftmax(nn.Module):
 
 
     def kl(self, dist_a):
-        return GumbelSoftmax._kld_categorical_uniform(
+        return torch.sum(GumbelSoftmax._kld_categorical_uniform(
             dist_a['discrete']['log_q_z'],
-        )
+        ), dim=-1)
 
     @staticmethod
     def _gumbel_softmax(x, tau, eps=1e-9, use_cuda=False):

models/reparameterizers/isotropic_gaussian.py
@@ -45,7 +45,7 @@ class IsotropicGaussian(nn.Module):
     def _kld_gaussian_N_0_1(mu, logvar):
         standard_normal = D.Normal(zeros_like(mu), ones_like(logvar))
         normal = D.Normal(mu, logvar)
-        return D.kl_divergence(normal, standard_normal)
+        return torch.sum(D.kl_divergence(normal, standard_normal), -1)
 
     def kl(self, dist_a):
         return IsotropicGaussian._kld_gaussian_N_0_1(

models/reparameterizers/mixture.py
@@ -62,7 +62,7 @@ class Mixture(nn.Module):
     def kl(self, dist_a):
         gauss_kl = self.gaussian.kl(dist_a)
         disc_kl = self.discrete.kl(dist_a)
-        return torch.cat([gauss_kl, disc_kl], dim=1)
+        return gauss_kl + disc_kl
 
     def forward(self, logits):
         return self.reparmeterize(logits)

models/student_teacher.py
@@ -162,18 +162,18 @@ class StudentTeacher(nn.Module):
 
         return vae_loss
 
-    def _ewc(self, fisher_matrix, gamma=30):
+    def _ewc(self, fisher_matrix):
         losses = []
         for (nt, pt), (ns, ps), (nf, fish) in zip(self.teacher.named_parameters(),
                                                   self.student.named_parameters(),
                                                   fisher_matrix.items()):
             # print("f {} * (t {} - s {})".format(nf, nt, ns))
             # print("f {} * (t {} - s {})".format(fish.size(), pt.size(), ps.size()))
-            # print("f {} * (t {} - s {})".format(type(fish), type(pt), type(ps)))
+            # print("f {} * (t {} - s {})".format(fish.type(), pt.type(), ps.type()))
             if pt.size() == ps.size():
-                losses.append(torch.sum(fish * (pt - ps)**2))
+                losses.append(torch.sum(fish * (ps - pt)**2))
 
-        return (gamma / 2.0) * sum(losses)
+        return (self.config['ewc_gamma'] / 2.0) * sum(losses)
 
 
     def _ewc_loss_function(self, output_map, fisher_matrix):

models/vae/abstract_vae.py
@@ -232,7 +232,7 @@ class AbstractVAE(nn.Module):
             # mut_info = self.config['mut_reg'] * mut_info
             mut_info = self.config['mut_reg'] * (mut_info / torch.norm(mut_info, p=2))
 
-        loss = elbo + mut_info
+        loss = elbo - mut_info
         return {
             'loss': loss,
             'loss_mean': torch.mean(loss),

models/vae/parallelly_reparameterized_vae.py
@@ -105,7 +105,7 @@ class ParallellyReparameterizedVAE(AbstractVAE):
 
     def kld(self, dist_a):
         ''' KL divergence between dist_a and prior '''
-        return torch.sum(self.reparameterizer.kl(dist_a), dim=-1)
+        return self.reparameterizer.kl(dist_a)
 
     def mut_info(self, dist_params):
         ''' helper to get mutual info '''